21d3f603506f39cc82df04588cdea88463f42140
giulianigabrie <giulianigb@gmail.com>
2021-11-26 10:35:16 -0300

    actualizacion

--- Funciones/_mysql_importador_geometrias.sql
+++ Funciones/_mysql_importador_geometrias.sql
@@ -6,54 +6,65 @@ CREATE OR REPLACE FUNCTION _mysql.importador_geometrias(
 	tabla character varying)
     RETURNS TABLE(cantidad integer) 
     LANGUAGE 'plpgsql'
-
     COST 100
-    VOLATILE 
+    VOLATILE PARALLEL UNSAFE
     ROWS 1000
+
 AS $BODY$
 BEGIN EXECUTE (
   '
 
 create table if not exists _mysql.pais (pais_id int, pais_descripcion text, pais_fecha_actualizacion text, pais_habilitado int, wkt text);
 
-TRUNCATE TABLE _MYSQL.PAIS;
-
 INSERT INTO _MYSQL.pais (wkt, PAIS_ID,PAIS_DESCRIPCION,PAIS_FECHA_ACTUALIZACION, PAIS_HABILITADO)
-select 	st_astext(st_transform(GEOM,4326)), id,pais,now() as FECHAMOD,''1'' from capas_gral.pais WHERE pais in (select DISTINCT(PAIS) from capas_gral.partidos where PROVINCIA = ''' || tabla || ''');
+SELECT st_astext(st_transform(GEOM,4326)), id,pais,now() as FECHAMOD,''1'' 
+	from capas_gral.pais 
+		WHERE pais in (select DISTINCT(PAIS) from capas_gral.partidos where PROVINCIA = ''' || tabla || ''') 
+		AND PAIS NOT IN (SELECT PAIS_DESCRIPCION FROM _MYSQL.pais);
 
 create table if not exists _mysql.provincia 
 (prov_id int, prov_pais_id int, prov_descripcion text, prov_fecha_actualizacion text,pais_habilitado int, wkt text);
 
-TRUNCATE TABLE _MYSQL.PROVINCIA;
-
 INSERT INTO _MYSQL.PROVINCIA (PROV_ID,prov_pais_id, PROV_DESCRIPCION,prov_fecha_actualizacion,wkt,pais_habilitado)
-SELECT IDSOFLEX_PROV::INT,B.ID,PROVINCIA,FECHAMOD,st_Astext(ST_TRANSFORM(A.GEOM,4326)),1 FROM capas_gral.provincias A INNER JOIN CAPAS_GRAL.PAIS B ON A.PAIS = B.PAIS
-WHERE a.pais in (select DISTINCT(PAIS) from capas_gral.partidos where PROVINCIA = ''' || tabla || ''') ORDER BY IDSOFLEX_PROV;
+SELECT IDSOFLEX_PROV::INT,B.ID,PROVINCIA,FECHAMOD,st_Astext(ST_TRANSFORM(A.GEOM,4326)),1 
+	FROM capas_gral.provincias A 
+	INNER JOIN CAPAS_GRAL.PAIS B ON A.PAIS = B.PAIS
+		WHERE a.pais in (select DISTINCT(PAIS) from capas_gral.partidos where PROVINCIA = ''' || tabla || ''') 
+		AND PROVINCIA NOT IN (SELECT PROV_DESCRIPCION FROM _MYSQL.PROVINCIA) ORDER BY IDSOFLEX_PROV;
 
 create table if not exists _mysql.departamento
 (depar_id int, depar_prov_id int, depar_descripcion text,depar_fecha_actualizacion text, depar_habilitado int, depar_archivo_dbf text, depar_fecha_archivo_dbf text, wkt text);
 
-TRUNCATE TABLE _MYSQL.DEPARTAMENTO;
-
- insert into _mysql.departamento  (DEPAR_PROV_ID,  depar_id,  depar_descripcion,  depar_fecha_actualizacion,  depar_habilitado,  depar_archivo_dbf,  wkt)   SELECT b.id,IDSOFLEX_PROV::INT,PROVINCIA,FECHAMOD,1,1,st_Astext(ST_TRANSFORM(A.GEOM,4326)) FROM capas_gral.partidos  a inner join capas_gral.pais b on a.pais = b.pais    where provincia = ''' || tabla || ''' and a.pais in (select DISTINCT(PAIS) from capas_gral.partidos where PROVINCIA = ''' || tabla || ''')  ORDER BY IDSOFLEX_PROV;
+INSERT into _mysql.departamento  (DEPAR_PROV_ID,  depar_id,  depar_descripcion,  depar_fecha_actualizacion,  depar_habilitado,  depar_archivo_dbf,  wkt)   
+	SELECT IDSOFLEX_PROV::INT,CONCAT(IDSOFLEX_PROV,IDSOFLEX_DPTO)::INT,PARTIDO,FECHAMOD,1,ARCH_ID,st_Astext(ST_TRANSFORM(A.GEOM,4326)) 
+	FROM capas_gral.partidos  a 
+	inner join capas_gral.pais b on a.pais = b.pais    
+		where provincia = ''' || tabla || ''' 
+		and a.pais in (select DISTINCT(PAIS) from capas_gral.partidos where PROVINCIA = ''' || tabla || ''')
+		AND PARTIDO NOT IN (SELECT depar_descripcion FROM _mysql.departamento)
+	ORDER BY IDSOFLEX_PROV;
 
 create table if not exists _mysql.localidad
 (loca_id int, loca_depar_id int, loca_descripcion text, loca_fecha_actualizacion text, loca_habilitado int, loca_archivo_dbf text, loca_fecha_archivo_dbf text, wkt text);
 
-TRUNCATE TABLE _MYSQL.LOCALIDAD;
-
 insert into _mysql.localidad (LOCA_DEPAR_ID, loca_id,loca_descripcion,loca_fecha_actualizacion,loca_habilitado,loca_fecha_archivo_dbf, wkt,loca_archivo_dbf)
-SELECT CONCAT(IDSOFLEX_PROV,idsoflex_DPTO)::INT,CONCAT(IDSOFLEX_PROV,idsoflex_local)::INT,localidad,fechamod,''1'',now()::timestamp::text,st_astext(st_transform(geom,4326)),''0'' FROM CAPAS_gRAL.LOCALIDADES WHERE PROVINCIA = ''' || tabla || ''' and localidad not like ''%ZONA RURAL%'' and idsoflex_local is not null
+SELECT CONCAT(IDSOFLEX_PROV,idsoflex_DPTO)::INT,CONCAT(IDSOFLEX_PROV,idsoflex_local)::INT,localidad,fechamod,''1'',now()::timestamp::text,st_astext(st_transform(a.geom,4326)),b.depar_archivo_dbf 
+	FROM CAPAS_gRAL.LOCALIDADES a
+	inner join _mysql.departamento b on b.depar_id = CONCAT(IDSOFLEX_PROV,idsoflex_DPTO)::INT
+		WHERE PROVINCIA = ''' || tabla || ''' 
+		AND localidad not like ''%ZONA RURAL%'' 
+		and idsoflex_local is not null
+		
+		and CONCAT(IDSOFLEX_PROV,idsoflex_local)::INT  NOT IN (select LOCA_ID from _mysql.localidad)
 ORDER BY idsoflex_local ASC;
 
 create table if not exists _mysql.archivosdbf 
 (id int,arch_id int, arch_nombre text, arch_fecha_modificacion text,arch_fecha_actualizacion text, wkt text );
 
-TRUNCATE TABLE _MYSQL.ARCHIVOSDBF;
-
 insert into _mysql.archivosdbf
 (id, arch_id, arch_nombre, arch_fecha_modificacion,arch_fecha_actualizacion,wkt)
-select id,arch_id,concat(shp,''.dbf'') as arch_nombre,now()::timestamp::text as arch_fecha_modificacion ,now()::timestamp::text as arch_fecha_actualizacion, st_Astext(st_transform(geom,4326))  from capas_gral.partidos where provincia = ''' || tabla || ''';
+select id,arch_id,lower(concat(shp,''.dbf'')) as arch_nombre,now()::timestamp::text as arch_fecha_modificacion ,now()::timestamp::text as arch_fecha_actualizacion, st_Astext(st_transform(geom,4326))  from capas_gral.partidos 
+where provincia = ''' || tabla || ''' and lower(concat(shp,''.dbf'')) not in (select arch_nombre from _mysql.archivosdbf);
 
 update _mysql.archivosdbf set geom = st_multi(st_setsrid(st_geomfromtext(wkt),4326)) where geom is null;
 update _mysql.departamento set geom = st_multi(st_setsrid(st_geomfromtext(wkt),4326)) where geom is null;
@@ -101,6 +112,3 @@ RETURN query execute (
 );
 END
 $BODY$;
-
-ALTER FUNCTION _mysql.importador_geometrias(character varying)
-    OWNER TO nahuel;
